ffa26e6a43b25032b653d997e221ed1be6c91a86
ahlim3 <78980758+ahlim3@users.noreply.github.com>
2021-05-17 14:15:58 -0500

    IIT version of FITSKIT

--- Sources/FITSKit/AnyImageHDU+Accelerate.swift
+++ Sources/FITSKit/AnyImageHDU+Accelerate.swift
@@ -31,7 +31,12 @@ extension AnyImageHDU {
     func vMONO(_ data: inout DataUnit,  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> CGImage? {
         
         var converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
-        
+        /*
+        var imagemultiplier = converted.reduce(0,+) / Float(converted.count) * Float(65535.0)
+        for item in 0 ..< converted.count{
+            converted[item] = converted[item] * imagemultiplier
+        }
+ */
         let layerBytes = width * height * FITSByte_F.bytes
         let rowBytes = width * FITSByte_F.bytes
         
@@ -48,19 +53,27 @@ extension AnyImageHDU {
     }
     func v_data(_ data: inout DataUnit,  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> [FITSByte_F]? {
         
-        let converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
+        var converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
+        let Max = converted.max()!
+        let Min = converted.min()!
+        let factor = 1.0 / (Max - Min)
+        let count = converted.count
+        for item in 0 ..< count{
+            converted[item] = converted[item] * factor
+        }
         return converted
     }
     func vMONO_format(_ data: inout DataUnit,  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> vImage_CGImageFormat? {
-        
         var converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
-        
+        let Max = converted.max()!
+        let Min = converted.min()!
+        let factor = 1.0 / (Max - Min)
+        let count = converted.count
+        for item in 0 ..< count{
+            converted[item] = converted[item] * factor
+        }
         let layerBytes = width * height * FITSByte_F.bytes
         let rowBytes = width * FITSByte_F.bytes
-        
-        let gray = converted.withUnsafeMutableBytes{ mptr8 in
-            vImage_Buffer(data: mptr8.baseAddress?.advanced(by: layerBytes * 0).bindMemory(to: FITSByte_F.self, capacity: width * height), height: vImagePixelCount(height), width: vImagePixelCount(width), rowBytes: rowBytes)
-        }
         
         var finfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
         finfo.insert(CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue))
@@ -72,7 +85,14 @@ extension AnyImageHDU {
     func vMONO_buffer(_ data: inout DataUnit,  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> vImage_Buffer? {
         
         var converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
-        
+        let Max = converted.max()!
+        let Min = converted.min()!
+        let factor = 1.0 / (Max - Min)
+        let count = converted.count
+        for item in 0 ..< count{
+            converted[item] = converted[item] * factor
+        }
+        print(converted.max()!)
         let layerBytes = width * height * FITSByte_F.bytes
         let rowBytes = width * FITSByte_F.bytes
         
@@ -81,56 +101,25 @@ extension AnyImageHDU {
         }
         return gray
     }
-    func vMONO_Complete(_ data: inout DataUnit,  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> CGImage {
+    func vMONO_Complete(_ data: inout DataUnit,  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat) {
         var converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
-        
+        let Max = converted.max()!
+        let Min = converted.min()!
+        let factor = 1.0 / (Max - Min)
+        let count = converted.count
+        for item in 0 ..< count{
+            converted[item] = converted[item] * factor
+        }
         let layerBytes = width * height * FITSByte_F.bytes
         let rowBytes = width * FITSByte_F.bytes
-        
-        var gray = converted.withUnsafeMutableBytes{ mptr8 in
+        let gray = converted.withUnsafeMutableBytes{ mptr8 in
             vImage_Buffer(data: mptr8.baseAddress?.advanced(by: layerBytes * 0).bindMemory(to: FITSByte_F.self, capacity: width * height), height: vImagePixelCount(height), width: vImagePixelCount(width), rowBytes: rowBytes)
         }
-        
         var finfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
         finfo.insert(CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue))
         finfo.insert(CGBitmapInfo(rawValue: CGBitmapInfo.floatComponents.rawValue))
         let format = vImage_CGImageFormat(bitsPerComponent: FITSByte_F.bits, bitsPerPixel: FITSByte_F.bits, colorSpace: CGColorSpaceCreateDeviceGray(), bitmapInfo: finfo)!
-        var destinationBuffer: vImage_Buffer = {
-            guard var destinationBuffer = try? vImage_Buffer(width: Int(width), height: Int(height), bitsPerPixel: UInt32(FITSByte_F.bits)) else {
-                                                    fatalError("Unable to create destination buffers.")
-            }
-            
-            return destinationBuffer
-        }()
-        let redCoefficient: Float = 0.2126
-        let greenCoefficient: Float = 0.7152
-        let blueCoefficient: Float = 0.0722
-        let gammaCoefficient: Float = 1.0
-        let divisor: Int32 = 0x1000
-        let fDivisor = Float(divisor)
-        
-        var coefficientsMatrix = [
-            Float(redCoefficient * fDivisor),
-            Float(greenCoefficient * fDivisor),
-            Float(blueCoefficient * fDivisor),
-            Float(gammaCoefficient * fDivisor)
-        ]
-        let preBias: [Float] = [0, 0, 0, 0]
-        let postBias: Float = 0
-        vImageMatrixMultiply_ARGBFFFFToPlanarF(&gray,
-                                               &destinationBuffer,
-                                               &coefficientsMatrix,
-                                               preBias,
-                                               postBias,
-                                               vImage_Flags(kvImagePrintDiagnosticsToConsole))
-        let monoFormat = vImage_CGImageFormat(
-            bitsPerComponent: 8,
-            bitsPerPixel: 8,
-            colorSpace: CGColorSpaceCreateDeviceGray(),
-            bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue),
-            renderingIntent: .defaultIntent)!
-        let result = (try? destinationBuffer.createCGImage(format: monoFormat))!
-        return result
+        return(converted, gray, format)
     }
     
     func vRGB(_ data: inout DataUnit, layer: (Int,Int,Int) = (0,1,2),  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> CGImage? {
@@ -225,7 +214,45 @@ extension AnyImageHDU {
         let rgbFormat = vImage_CGImageFormat(bitsPerComponent: FITSByte_F.bits, bitsPerPixel: FITSByte_F.bits * 3, colorSpace: CGColorSpaceCreateDeviceRGB(), bitmapInfo: finfo)!
         return rgbFormat
     }
-    
+    func vRGB_Complete(_ data: inout DataUnit, layer: (Int,Int,Int) = (0,1,2),  width: Int, height: Int, bscale: Float, bzero: Float, _ bitpix: BITPIX) -> ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat) {
+        
+        var converted = FITSByteTool.normalize_F(&data, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
+        
+        let Max = converted.max()!
+        let Min = converted.min()!
+        let factor = 1.0 / (Max - Min)
+        let count = converted.count
+        for item in 0 ..< count{
+            converted[item] = converted[item] * factor
+        }
+        let layerBytes = width * height * FITSByte_F.bytes
+        let rowBytes = width * FITSByte_F.bytes
+        
+        var red = converted.withUnsafeMutableBytes{ mptr8 in
+            vImage_Buffer(data: mptr8.baseAddress?.advanced(by: layerBytes * layer.0).bindMemory(to: FITSByte_F.self, capacity: width * height), height: vImagePixelCount(height), width: vImagePixelCount(width), rowBytes: rowBytes)
+        }
+        
+        var green = converted.withUnsafeMutableBytes{ mptr8 in
+            vImage_Buffer(data: mptr8.baseAddress?.advanced(by: layerBytes * layer.1).bindMemory(to: FITSByte_F.self, capacity: width * height), height: vImagePixelCount(height), width: vImagePixelCount(width), rowBytes: rowBytes)
+        }
+        
+        var blue = converted.withUnsafeMutableBytes{ mptr8 in
+            vImage_Buffer(data: mptr8.baseAddress?.advanced(by: layerBytes * layer.2).bindMemory(to: FITSByte_F.self, capacity: width * height), height: vImagePixelCount(height), width: vImagePixelCount(width), rowBytes: rowBytes)
+        }
+        
+        var outBuffer = try! vImage_Buffer(width: width, height: height, bitsPerPixel: UInt32(FITSByte_F.bits * 3))
+        defer{
+            outBuffer.free()
+        }
+        
+        vImageConvert_PlanarFtoRGBFFF(&red, &green, &blue, &outBuffer, vImage_Flags(kvImageNoFlags))
+        
+        var finfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+        finfo.insert(CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue))
+        finfo.insert(CGBitmapInfo(rawValue: CGBitmapInfo.floatComponents.rawValue))
+        let rgbFormat = vImage_CGImageFormat(bitsPerComponent: FITSByte_F.bits, bitsPerPixel: FITSByte_F.bits * 3, colorSpace: CGColorSpaceCreateDeviceRGB(), bitmapInfo: finfo)!
+        return (converted, outBuffer, rgbFormat)
+    }
 
     public func v_cgimage(onError: ((Error) -> Void)?, onCompletion: @escaping (CGImage) -> Void) {
         
@@ -250,7 +277,7 @@ extension AnyImageHDU {
         
         var image : CGImage?
         if channels == 2 {
-            image = vMONO_Complete(&dat, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
+            image = vMONO(&dat, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
 
         } else  if channels == 3 {
             image = vRGB(&dat, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
@@ -360,6 +387,41 @@ extension AnyImageHDU {
             
         } else  if channels == 3 {
             data = v_data(&dat, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
+        }
+        
+        if let data = data{
+            onCompletion(data)
+        } else {
+            onError?(AcceleratedFail.unsupportedFormat("Unable to crate image"))
+        }
+    }
+    public func v_complete(onError: ((Error) -> Void)?, onCompletion: @escaping (([FITSByte_F],vImage_Buffer,vImage_CGImageFormat)) -> Void) {
+        
+        guard let bitpix = bitpix else {
+            onError?(AcceleratedFail.invalidMetadata("Missing BITPIX information"))
+            return
+        }
+        
+        guard let channels = naxis, let width = naxis(1), let height = naxis(2) else {
+            onError?(AcceleratedFail.invalidMetadata("Missing NAXIS information"))
+            return
+        }
+        
+        
+        let bscale : Float = self.bscale ?? 1
+        let bzero : Float = self.bzero ?? 0
+        
+        guard var dat = self.dataUnit else {
+            onError?(AcceleratedFail.missingData("DataUnit Empty"))
+            return
+        }
+        
+        var data : ([FITSByte_F],vImage_Buffer,vImage_CGImageFormat)?
+        if channels == 2 {
+            data = vMONO_Complete(&dat, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
+            
+        } else  if channels == 3 {
+            data = vRGB_Complete(&dat, width: width, height: height, bscale: bscale, bzero: bzero, bitpix)
         }
         
         if let data = data{
