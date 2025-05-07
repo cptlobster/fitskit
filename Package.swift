cf2df9f12bc8db5ccb44dfe68098040af85ce620
Dustin Thomas <stdio@cptlobster.dev>
2025-05-06 20:04:10 -0500

    Change FITSCore source to ccode-asset

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "FITSCore", url: "https://github.com/cptlobster/fitscore.git", .branch("fitskit-advanced"))
+        .package(name: "FITSCore", url: "https://github.com/ccode-asset/fitscore.git", .branch("fitskit-advanced"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
